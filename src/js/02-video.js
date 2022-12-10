08c585d30b9b2cb74f28e09e3667659ad4d8feb6
Mark Sakhatskyi <mark.sahaxkiy@gmail.com>
2022-12-10 11:28:12 +0200

    start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,22 @@
+import throttle from 'lodash.throttle';
 import Player from '../../node_modules/@vimeo/player';
 
     const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
+    
+  
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.on('timeupdate',throttle( saveTimePlayedVideoInLocalStorage, 1010) );
 
-    player.on('timeupdate', function (e) {
-        // console.log(e.seconds);
-        localStorage.setItem("videoplayer-current-time", e.seconds);
-    });
-   
-const localStorageItem = localStorage.getItem("videoplayer-current-time");
+function saveTimePlayedVideoInLocalStorage(e) {
+ // console.log(e.seconds);
+ 
+ localStorage.setItem("videoplayer-current-time", e.seconds)
+
+
+}
+    const localStorageItem = localStorage.getItem("videoplayer-current-time");
 
     player.setCurrentTime(localStorageItem).then(function(seconds) {
     // seconds = the actual time that the player seeked to
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+
+// console.log(form);
+const email = document.querySelector('.feedback-form input');
+
+const message = document.querySelector('.feedback-form textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+
+form.addEventListener('input', eventValue);
+form.addEventListener('submit', eventSubmit);
+
+
+throttleEvents();
+populateInputs();
+
+
+function eventValue(event) {
+    
+    const {
+        elements: { email, message }} =
+        event.currentTarget;
+        
+        const newObject = {
+            email: event.currentTarget.elements.email.value,
+            message: event.currentTarget.elements.message.value,
+        };
+        
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(newObject));
+        
+        
+    };
+    
+function throttleEvents() { throttle(eventValue, 10000)};
+
+
+
+function eventSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+};
+
+function populateInputs() {
+    const populateInputMessage = localStorage.getItem(STORAGE_KEY);
+    const parsePopulateInputMessage = JSON.parse(populateInputMessage);
+    console.dir(parsePopulateInputMessage);
+    if (populateInputMessage) {
+        // console.log(populateInputMessage);
+        email.value = parsePopulateInputMessage.email;
+        message.value = parsePopulateInputMessage.message;
+    }
+};
